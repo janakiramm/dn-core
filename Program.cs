d610edab37ed4025648844fae0aaf9a4644fa45f
Janakiram MSV <janakiramm@users.noreply.github.com>
2017-08-15 11:35:34 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace DSLibrary
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseUrls("http://0.0.0.0:5000/")
                 .Build();
 
             host.Run();
